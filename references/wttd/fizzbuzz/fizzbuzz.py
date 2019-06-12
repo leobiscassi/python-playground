ea8068e036c7cf81c792f95c9bba5a448ce80e63
Léo Biscassi <leo.biscassi@gmail.com>
2019-06-12 19:07:44 -0300

    Adiciona testes com unittest

--- references/wttd/fizzbuzz/fizzbuzz.py
+++ references/wttd/fizzbuzz/fizzbuzz.py
@@ -22,31 +22,3 @@ def robot(pos):
     elif multiple_of_3(pos):
         say = 'fizz'
     return say
-
-
-def assert_equal(result, expected):
-    from sys import _getframe
-    if not result == expected:
-        current = _getframe()
-        caller = current.f_back
-        line_no = caller.f_lineno
-
-        print(f'Fail: Line {line_no} got {result} expecting {expected}')
-
-
-if __name__ == '__main__':
-    assert_equal(robot(1), '1')
-    assert_equal(robot(2), '2')
-    assert_equal(robot(4), '4')
-
-    assert_equal(robot(3), 'fizz')
-    assert_equal(robot(6), 'fizz')
-    assert_equal(robot(9), 'fizz')
-
-    assert_equal(robot(5), 'buzz')
-    assert_equal(robot(10),  'buzz')
-    assert_equal(robot(20),  'buzz')
-
-    assert_equal(robot(15), 'fizzbuzz')
-    assert_equal(robot(30), 'fizzbuzz')
-    assert_equal(robot(45), 'fizzbuzz')
